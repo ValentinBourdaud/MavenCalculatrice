23920cff813adcf6b6ae9fcdb714fe47b4411416
Valentin Bourdaud <bourdaudv@gmail.com>
2018-04-12 17:36:14 +0200

    Création Maven Calculatrice

---
+++ src/main/java/Calculatrice.java
@@ -0,0 +1,25 @@
+
+public class Calculatrice {
+		
+	private Calculatrice calculatrice;
+	
+	private double a;
+	private double b;
+	
+	Calculatrice calculatrice1 = new Calculatrice();
+	
+	double additionner (double a, double b) {
+		return a+b;
+	}
+	double soustraire (double a, double b) {
+		return a-b;
+	}
+	double multiplier (double a, double b) {
+		return a*b;
+	}
+	double diviser (double a, double b) {
+		return a/b;
+	}
+	
+	
+}
