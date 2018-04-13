01e3de660276da4d147454359a60f72deb9879f1
Valentin Bourdaud <bourdaudv@gmail.com>
2018-04-13 09:47:01 +0200

    Calculatrice terminée et commentée

--- src/main/java/Calculatrice.java
+++ src/main/java/Calculatrice.java
@@ -6,17 +6,30 @@ public class Calculatrice {
 	private double a;
 	private double b;
 	
+	//Creation de l'objet calculatrice
+	
 	Calculatrice calculatrice1 = new Calculatrice();
+	
+	//Fonction additionner
 	
 	public static double additionner (double a, double b) {
 		return a+b;
 	}
+	
+	//Fonction soustraire
+	
 	public static double soustraire (double a, double b) {
 		return a-b;
 	}
+	
+	//Fonction multiplier
+	
 	public static double multiplier (double a, double b) {
 		return a*b;
 	}
+	
+	//Fonction diviser
+	
 	public static double diviser (double a, double b) {
 		return a/b;
 	}
